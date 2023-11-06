f4c998a1c31e0691dafbba26f2a944d7309d6351
Musfarr <musfarhassan@gmail.com>
2023-11-06 18:41:16 +0500

    grids adjust

--- src/components/form.js
+++ src/components/form.js
@@ -6,16 +6,13 @@ const Form = () => {
     <div className="bg">
       <div className="formhead  ">
         <div className="uk-container uk-container-large">
-          <div
-            className="uk-flex uk-flex-between uk-flex-middle"
-            style={{ marginTop: "1.5vh" }}
-          >
-            <div className="  ">
+          <div className="uk-flex uk-flex-between uk-flex-middle">
+            <div>
               <img src={l1}></img>
               <span className="logotext">Berry Talks </span>
             </div>
 
-            <div className=" ">
+            <div>
               <p className="greenhead1 ">Need help</p>
             </div>
           </div>
@@ -34,64 +31,107 @@ const Form = () => {
             <h1 className="card2head uk-padding-remove-top">
               User information{" "}
             </h1>
-            <div className="uk-grid" uk-grid="">
-              <div className="uk-width-1-2">
-                {" "}
-                <p className="mbr card2body">First Name</p>
-                <input
-                  placeholder="First Name"
-                  className="uk-input-primary forminput"
-                />{" "}
-              </div>
-              <div className="uk-width-1-2">
-                <p className="mbr card2body">Last Name</p>
+            <form>
+            <div className="uk-container-small">
 
-                <input
-                  className="uk-input-primary forminput"
-                  placeholder="Last Name"
-                />
+              <div className="uk-grid uk-grid-small" uk-grid="">
+                <div className="uk-width-1-2">
+                  <label htmlFor="First" className="mbr card2body">
+                    {" "}
+                    First Name{" "}
+                  </label>
+                  <br />
+                  <input
+                    id="First"
+                    placeholder="First Name"
+                    className="uk-input forminput"
+                  />
+                </div>
+                <div className="uk-width-1-2">
+                  <label htmlFor="Last" className="mbr card2body">
+                    {" "}
+                    Last Name{" "}
+                  </label>
+                  <input
+                    id="Last"
+                    className="uk-input forminput"
+                    placeholder="Last Name"
+                  />
+                </div>
+                <div className="uk-width-1-2">
+                  <label htmlFor="Last" className="mbr card2body">
+                    {" "}
+                    Email{" "}
+                  </label>
+                  <input
+                    id="Last"
+                    className="uk-input forminput"
+                    placeholder="Email"
+                  />
+                </div>
+                <div className="uk-width-1-2">
+                  <label htmlFor="Last" className="mbr card2body">
+                    {" "}
+                    Mobile number{" "}
+                  </label>
+                  <input
+                    id="Last"
+                    className="uk-input forminput"
+                    placeholder="Mobile number"
+                  />
+                </div>
               </div>
-            </div>
+              
+              {/* <div className="uk-grid" uk-grid="">
+                <div className="uk-width-1-2">
+                  <label htmlFor="Email" className="mbr card2body">
+                    {" "}
+                    Email{" "}
+                  </label>
+                  <br />
+                  <input
+                    id="Email"
+                    placeholder="Email address  "
+                    className="uk-input-primary forminput"
+                  />
+                </div>
+                <div className="uk-width-1-2">
+                  <label htmlFor="mobile" className="mbr card2body">
+                    {" "}
+                    Mobile number{" "}
+                  </label>
+                  <input
+                    id="mobile"
+                    placeholder="Mobile"
+                    className="uk-input-primary forminput"
+                  />
+                </div>
+              </div> */}
 
-            <div className="uk-grid" uk-grid="">
-              <div className="uk-width-1-2">
-                {" "}
-                <p className="mbr card2body">Email </p>
-                <input
-                  placeholder="Email address  "
-                  className="uk-input-primary forminput"
-                />{" "}
+              <div className="uk-grid " uk-grid="">
+                <div className="uk-width-1-1">
+                  <p className="card2body mbr">Please briefly describe your requirements</p>
+                  <textarea
+                    id="textarea"
+                    rows="5"
+                    placeholder="Brief description..."
+                    className="uk-width-1-1  textbox forminput "
+                  />
+                  <p className="card2body mbr">
+                    Once you have submitted the form, our team will review your
+                    application and get back to you with further instructions.
+                  </p>
+                </div>
               </div>
-              <div className="uk-width-1-2">
-                <p className="mbr card2body">Mobile number</p>
-                <input
-                  placeholder="Mobile"
-                  className="uk-input-primary forminput"
-                />
               </div>
-            </div>
 
-            <div className="uk-grid " uk-grid="">
-              <div className="uk-width-1-1">
-                <p className="mbr card2body">
-                  Please briefly describe your requirements
-                </p>
-                <textarea
-                  rows="5"
-                  placeholder="Brief description..."
-                  className="uk-width-1-1 forminput textbox "
-                />
-                <p className="card2body mbr">
-                  Once you have submitted the form, our team will review your
-                  application and get back to you with further instructions.
-                </p>
+              <div className="frmbtn uk-flex  uk-align-right uk-margin-large">
+                <button className=" wbtn">Cancel </button>
+                <button type="submit" className="  loginbtn">
+                  Request a demo
+                </button>
               </div>
-            </div>
-
-            <div className="frmbtn uk-flex  uk-align-right uk-margin-large">
-              <button className=" wbtn">Cancel </button>
-              <button className="  loginbtn">Request a demo </button>
-            </div>
+            </form>
           </div>
         </div>
       </div>
